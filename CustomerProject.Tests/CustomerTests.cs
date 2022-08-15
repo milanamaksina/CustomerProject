52ad50791fd7c91eb0ff599bdc6a3e74c35e3a0a
Milana Maksina <m_maksinaa@mail.ru>
2022-08-15 12:07:12 +0300

    feature: add address validator tests

--- CustomerProject.Tests/CustomerTests.cs
+++ CustomerProject.Tests/CustomerTests.cs
@@ -1,12 +1,15 @@
 using CustomerProject.Entities;
 using CustomerProject.Validators;
+using FluentValidation.TestHelper;
 using System.Collections.Generic;
 
 namespace CustomerProject.Tests
 {
     public class CustomerTests
     {
-        private CustomerValidator _validator = new CustomerValidator();
+        private CustomerValidator _customerValidator = new CustomerValidator();
+        private AddressValidator _addressValidator = new AddressValidator();
+
 
         [Fact]
         public void ShouldBeAbleToCreateCustomer()
@@ -36,6 +39,66 @@ namespace CustomerProject.Tests
             Assert.Equal(testNotes, actualCustomer.Notes);
             Assert.Equal(1, actualCustomer.TotalPurchasesAmount);
         }
+
+        [Fact]
+        public void WhenAddressModelIsNotCorrect_ShouldThrowExceptions()
+        {
+            List<Address> customerAddresses = new List<Address>();
+            Address address = new Address("", "", AddressType.Shipping, "", "", "89236723262732", "France");
+
+            var result = _addressValidator.TestValidate(address);
+            result.ShouldHaveValidationErrorFor(x => x.AddressLine);
+            result.ShouldHaveValidationErrorFor(x => x.AddressLine);
+            result.ShouldHaveValidationErrorFor(x => x.City);
+            result.ShouldHaveValidationErrorFor(x => x.PostalCode);
+            result.ShouldHaveValidationErrorFor(x => x.Country);
+        }
+
+        [Fact]
+        public void TotalPurchasesAmountCanBeNull()
+        {
+            List<Address> customerAddresses = new List<Address>();
+            Address address = new Address("Pearl Street", "23/12", AddressType.Shipping, "New-York", "234213", "New-York", "USA");
+            customerAddresses.Add(address);
+            List<string> testNotes = new List<string>();
+            testNotes.Add("note");
+
+            Customer actualCustomer = new Customer()
+            {
+                FirstName = "Alex",
+                LastName = "Smith",
+                Addresses = customerAddresses,
+                PhoneNumber = "+195534912",
+                Email = "alex@gmail.com",
+                Notes = testNotes,
+                TotalPurchasesAmount = 0
+            };
+
+            Assert.Equal(0, actualCustomer.TotalPurchasesAmount);
+        }
+
+        [Fact]
+        public void WhenCustomerModelIsNotCorrect_ShouldThrowExceptions()
+        {
+            Customer actualCustomer = new Customer()
+            {
+                FirstName = "Alex",
+                LastName = "",
+                Addresses = new List<Address>(),
+                PhoneNumber = "+19",
+                Email = "alexcom",
+                Notes = new List<string>(),
+                TotalPurchasesAmount = 0
+            };
+
+            var result = _customerValidator.TestValidate(actualCustomer);
+            result.ShouldHaveValidationErrorFor(x => x.LastName);
+            result.ShouldHaveValidationErrorFor(x => x.Addresses);
+            result.ShouldHaveValidationErrorFor(x => x.PhoneNumber);
+            result.ShouldHaveValidationErrorFor(x => x.Email);
+            result.ShouldHaveValidationErrorFor(x => x.Notes);
+        }
+
 
 
 
--- CustomerProject/Validators/AddressValidator.cs
+++ CustomerProject/Validators/AddressValidator.cs
@@ -11,26 +11,25 @@ namespace CustomerProject.Validators
         public AddressValidator()
         {
             var availableCountries = new List<string> {"USA", "Canada"};
-            RuleFor(c => c.AddressLine)
+            RuleFor(a => a.AddressLine)
                .NotEmpty().WithMessage(CustomErrorMessage.AddressLineIsRequared)
                .MaximumLength(100).WithMessage(CustomErrorMessage.AddressLineLenghtException);
-            RuleFor(c => c.AddressLine2)
+            RuleFor(a => a.AddressLine2)
                .MaximumLength(100).WithMessage(CustomErrorMessage.AddressLine2LenghtException);
-            RuleFor(c => c.AddressType)
+            RuleFor(a => a.AddressType)
                .IsInEnum().WithMessage(CustomErrorMessage.AddressTypeIsNotEnum);
-            RuleFor(c => c.City)
+            RuleFor(a => a.City)
                .NotEmpty().WithMessage(CustomErrorMessage.CityIsRequared)
                .MaximumLength(50).WithMessage(CustomErrorMessage.CityLenghtException);
-            RuleFor(c => c.PostalCode)
+            RuleFor(a => a.PostalCode)
                .NotEmpty().WithMessage(CustomErrorMessage.PostalCodeIsRequared)
                .MaximumLength(6).WithMessage(CustomErrorMessage.PostalCodeLenghtException);
-            RuleFor(c => c.State)
+            RuleFor(a => a.State)
                .NotEmpty().WithMessage(CustomErrorMessage.StateIsRequared)
                .MaximumLength(20).WithMessage(CustomErrorMessage.StateLenghtException);
-            RuleFor(c => c.Country)
+            RuleFor(a => a.Country)
               .NotEmpty().WithMessage(CustomErrorMessage.CountryIsRequared)
-              .Must(c => availableCountries.Contains(c)).WithMessage(CustomErrorMessage.InvalidCountryName);
-              //.Must(c=>c.Equals("USA")||c.Equals("Canada"));
+              .Must(a => availableCountries.Contains(a)).WithMessage(CustomErrorMessage.InvalidCountryName);
         }
 
 
