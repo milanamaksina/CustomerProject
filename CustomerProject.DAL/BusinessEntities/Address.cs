1394a990807a846da5d8bf5cc8fab305e486a006
Milana Maksina <m_maksinaa@mail.ru>
2022-08-22 22:13:09 +0300

    add: address repository

--- CustomerProject.DAL/BusinessEntities/Address.cs
+++ CustomerProject.DAL/BusinessEntities/Address.cs
@@ -4,9 +4,10 @@ namespace CustomerProject.DAL.BusinessEntities
 {
     public class Address
     {
+        public int AddressId { get; set; }
         public string AddressLine { get; set; }
         public string AddressLine2 { get; set; }
-        public AddressType AddressType { get; set; }
+        public string AddressType { get; set; }
         public string City { get; set; }
         public string PostalCode { get; set; }
         public string State { get; set; }
--- CustomerProject.DAL/Repositories/AddressRepository.cs
+++ CustomerProject.DAL/Repositories/AddressRepository.cs
@@ -1,4 +1,5 @@
 ﻿using CustomerProject.DAL.BusinessEntities;
+using System.Data.SqlClient;
 
 namespace CustomerProject.DAL.Repositories
 {
@@ -6,22 +7,110 @@ namespace CustomerProject.DAL.Repositories
     {
         public void Create(Address entity)
         {
-            throw new NotImplementedException();
+            using var connection = GetConnection();
+            connection.Open();
+            var command = new SqlCommand(
+                "INSERT INTO Addresses(Line, Line2, AddressType, City, PostalCode, State, Country)" +
+                "VALUES (@Line, @Line2, @AddressType, @City, @PostalCode, @State, @Country)",
+                connection);
+
+            var addressLine = new SqlParameter("@Line", System.Data.SqlDbType.NVarChar, 100)
+            {
+                Value = entity.AddressLine
+            };
+            var addressLine2 = new SqlParameter("@Line2", System.Data.SqlDbType.NVarChar, 50)
+            {
+                Value = entity.AddressLine2
+            };
+            var addressAddressType = new SqlParameter("@AddressType", System.Data.SqlDbType.NVarChar, 8)
+            {
+                Value = entity.AddressType
+            };
+            var addressCity = new SqlParameter("@City", System.Data.SqlDbType.NVarChar, 50)
+            {
+                Value = entity.City
+            };
+            var addressPostalCode = new SqlParameter("@PostalCode", System.Data.SqlDbType.NVarChar, 6)
+            {
+                Value = entity.PostalCode
+            };
+            var addressState = new SqlParameter("@State", System.Data.SqlDbType.NVarChar, 20)
+            {
+                Value = entity.State
+            };
+            var addressCountry = new SqlParameter("@State", System.Data.SqlDbType.NVarChar, 14)
+            {
+                Value = entity.Country
+            };
+            command.Parameters.Add(addressLine);
+            command.Parameters.Add(addressLine2);
+            command.Parameters.Add(addressAddressType);
+            command.Parameters.Add(addressCity);
+            command.Parameters.Add(addressPostalCode);
+            command.Parameters.Add(addressState);
+            command.Parameters.Add(addressCountry);
+            command.ExecuteNonQuery();
         }
 
         public void Delete(Address entity)
         {
-            throw new NotImplementedException();
+            using var connection = GetConnection();
+            connection.Open();
+            var command = new SqlCommand("DELETE FROM [Addresses] WHERE AddressId = @AddressId", connection);
+            var addressId = new SqlParameter("@AddressId", System.Data.SqlDbType.Int)
+            {
+                Value = entity.AddressId
+            };
+            command.Parameters.Add(addressId);
+            command.ExecuteNonQuery();
         }
 
         public Address Read(Address entity)
         {
-            throw new NotImplementedException();
+            using var connection = GetConnection();
+            connection.Open();
+            var command = new SqlCommand("SELECT * FROM [Customers] WHERE AddressId = @AddressId", connection);
+            var customerId = new SqlParameter("@AddressId", System.Data.SqlDbType.Int)
+            {
+                Value = entity.AddressId
+            };
+            command.Parameters.Add(customerId);
+            using var reader = command.ExecuteReader();
+            if (reader.HasRows)
+            {
+                return new Address
+                {
+                    AddressLine = reader["Line"].ToString(),
+                    AddressLine2 = reader["AddressLine2"].ToString(),
+                    AddressType = reader["AddressType"].ToString(),
+                    City = reader["City"].ToString(),
+                    PostalCode = reader["PostalCode"].ToString(),
+                    State = reader["State"].ToString(),
+                    Country = reader["Country"].ToString()
+
+
+                };
+            }
+            reader.Close();
+            return null;
         }
 
         public void Update(Address entity)
         {
-            throw new NotImplementedException();
+            using var connection = GetConnection();
+            connection.Open();
+            var command = new SqlCommand("UPDATE [Addresses] SET Line = @Line WHERE AddressId = @AddressId", connection);
+            var addressId = new SqlParameter("@AddressId", System.Data.SqlDbType.Int)
+            {
+                Value = entity.AddressId
+            };
+            var customerLine = new SqlParameter("@Line", System.Data.SqlDbType.NVarChar, 50)
+            {
+                Value = entity.AddressLine
+            };
+            command.Parameters.Add(addressId);
+            command.Parameters.Add(customerLine);
+            command.ExecuteNonQuery();
         }
     }
 }
