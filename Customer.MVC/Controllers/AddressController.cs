97194f8809eb451dbc5e5fc7965bf826aa3f3660
Milana Maksina <m_maksinaa@mail.ru>
2022-08-30 13:17:54 +0300

    feat: add mvc proj

---
+++ Customer.MVC/Controllers/AddressController.cs
@@ -0,0 +1,99 @@
+﻿using CustomerProject.DAL.BusinessEntities;
+using CustomerProject.DAL.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Customer.MVC.Controllers
+{
+    public class AddressController : Controller
+    {
+        private readonly IRepository<Address> _addressRepository;
+
+        public AddressController()
+        {
+            _addressRepository = new AddressRepository();
+        }
+
+        // GET: Address
+        public ActionResult Index()
+        {
+            var address = _addressRepository.GetAll();
+            return View(address);
+        }
+
+        // GET: Address/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Address/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Address/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Address/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Address/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Address/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Address/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
